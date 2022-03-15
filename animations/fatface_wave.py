1a7d5c1eeb516983e0c3080512bab1499d9e3a6d
Rob Stenson <rob.stenson@gmail.com>
2022-03-15 15:33:11 -0700

    downgrade walrus operator in fatface_wave.py

--- animations/fatface_wave.py
+++ animations/fatface_wave.py
@@ -3,10 +3,10 @@ from coldtype import *
 @animation((1080, 1080/2), timeline=50)
 def fatface_wave(f):
     return (Glyphwise("WAVEFORM", lambda g:
-        (Style("OhnoFatfaceVariable",
-            100+(fa:=f.adj(g.i*4)).e(ease:="seio", 1)*150,
-            wdth=fa.e(ease, 1),
-            opsz=fa.e(ease, 1),
-            rotate=-15+30*fa.e(ease, 1))))
+        (Style("OhnoFatfaceVariable"
+            , fontSize=f.adj(g.i*4).e("seio", 1, r=(100, 250))
+            , wdth=f.adj(g.i*4).e("seio", 1)
+            , opsz=f.adj(g.i*4).e("seio", 1)
+            , rotate=f.adj(g.i*4).e("seio", 1, r=(-20, 20)))))
         .align(f.a.r, h=200)
         .f(0))
