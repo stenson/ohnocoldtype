ee84b0090304715eb5f0bc03d6f61e86fb541eca
Rob Stenson <rob.stenson@gmail.com>
2022-03-15 14:32:38 -0700

    add an ss01 toggle animation to variable.py

--- animations/variable.py
+++ animations/variable.py
@@ -5,6 +5,7 @@ at = AsciiTimeline(4, 30, """
 [w             ]
        [s           ]
                         [w  ]
+            [ss01     ]
 """)
 
 txt = "VARIABLE"
@@ -12,6 +13,7 @@ txt = "VARIABLE"
 @animation((1080, 540), tl=at, bg=1, render_bg=1)
 def variable(f):
     return (StSt(txt, "IrregardlessV", 300
+        , ss01=at.ki("ss01").on()
         , wght=at.ki("w").io(10)
         , slnt=at.ki("s").io(10))
         .align(f.a.r, th=0)
